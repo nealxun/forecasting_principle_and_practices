4bb63865fe0fb9898426683968dded316ccd584e
robjhyndman <robjhyndman@gmail.com>
2017-01-05 12:33:58 +1100

    pdf can now be produced (badly)

--- makebook.R
+++ makebook.R
@@ -1,3 +1,15 @@
-bookdown::render_book("index.Rmd", "bookdown::gitbook")
-warnings()
-#rmarkdown::render_site(output_format = 'bookdown::gitbook', encoding = 'UTF-8')
+formats <- commandArgs(TRUE)
+
+options(warn=1)
+
+# provide default formats if necessary
+if (length(formats) == 0)
+	formats <- c('bookdown::pdf_book', 'bookdown::gitbook')
+
+# render the book to all formats unless they are specified via command-line args
+for (fmt in formats)
+  bookdown::render_book("index.Rmd",fmt,quiet=FALSE)
+
+# Publish to OTexts
+#if (length(formats) > 1) bookdown::publish_book()
+

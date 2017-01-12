499bc5c4b0b6d449e64b2bbb06dd533c3b3b711f
Rob J Hyndman <robjhyndman@gmail.com>
2017-01-12 17:39:36 +1100

    Cleaned up makebook.R

--- makebook.R
+++ makebook.R
@@ -6,10 +6,9 @@ options(warn=1)
 if (length(formats) == 0)
 	formats <- "bookdown::gitbook" #c('bookdown::pdf_book', 'bookdown::gitbook')
 
-# render the book to all formats unless they are specified via command-line args
+# render the book to all formats
 for (fmt in formats)
   bookdown::render_book("index.Rmd",fmt,quiet=FALSE)
 
 # Publish to OTexts
 #if (length(formats) > 1) bookdown::publish_book()
-

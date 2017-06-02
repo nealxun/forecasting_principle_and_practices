5ac4d40134a80ae464971846f4e1490327eb58ac
Rob J Hyndman <robjhyndman@gmail.com>
2017-06-02 16:19:25 +1000

    Updated pre-chapter code

--- before-each-chapter.R
+++ before-each-chapter.R
@@ -1,3 +1,21 @@
 rm(list=ls(all=TRUE))
+
+set.seed(1967)
+
+# Copy some Hadley code from r4ds
+options(digits = 3)
+knitr::opts_chunk$set(
+  comment = "#>",
+  collapse = TRUE,
+  echo = TRUE,
+  cache = TRUE,
+  out.width = "70%",
+  fig.align = 'center',
+  fig.width = 6,
+  fig.asp = 0.618,  # 1 / phi
+  fig.show = "hold"
+)
+
+# Everything depends on fpp2
 library(fpp2, quietly=TRUE)
 
